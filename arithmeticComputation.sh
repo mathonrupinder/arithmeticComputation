98351baba98d6283bd8b7a0a477f2fda353b4461
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-09 22:45:33 +0530

    Added:Three Inputs from user

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,1 +1,7 @@
 echo "Welcome to arithmetic computation and sorting program"
+read -p "Enter first input:" a
+read -p "Enter second input:" b
+read -p "Enter third input:" c
+echo "$a"
+echo "$b"
+echo "$c"
