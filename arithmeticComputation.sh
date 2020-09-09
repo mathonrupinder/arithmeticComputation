defba4f6bc076113279e1d672d1f21aafc06327c
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-09 23:24:30 +0530

    Added: Store results Dictionary and array

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -2,14 +2,18 @@ echo "Welcome to arithmetic computation and sorting program"
 read -p "Enter first input:" a
 read -p "Enter second input:" b
 read -p "Enter third input:" c
-compute1=$(( a+b*c ))
-echo "computation1=$compute1"
-compute2=$(( a*b+c ))
-echo "computation2=$compute2"
-compute3=$(( c+a/b ))
-echo "computation3=$compute3"
-compute4=$(( a%b+c ))
-echo "computation4=$compute4"
+declare -A compute
+compute[1]=$(( a+b*c ))
+compute[2]=$(( a*b+c ))
+compute[3]=$(( c+a/b ))
+compute[4]=$(( a%b+c ))
+arr=()
+for(( i=1; i<=4; i++ ))
+do
+   echo "computation[$i]:${compute[$i]}"
+   arr[i]=${compute[$i]}
+   
+done
 
 
 
